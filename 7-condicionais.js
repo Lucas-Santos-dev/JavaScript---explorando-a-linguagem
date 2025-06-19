ee4846968f5195f50f90669df4d6d4e52ac4e8e8
Lucas-Santos-dev <lucassantosrocha889@gmail.com>
2025-06-19 00:51:50 -0300

    ifs encadeados

--- 7-condicionais.js
+++ 7-condicionais.js
@@ -6,7 +6,8 @@ const listaDeDestinos = new Array(
     `Rio de Janeiro`
 );
 
-const idadeComprador = 15;
+const idadeComprador = 18;
+const estaAcompanhada = true;
 
 console.log(`Destinos Possíveis`);
 console.log(listaDeDestinos);
@@ -14,8 +15,10 @@ console.log(listaDeDestinos);
 if (idadeComprador >= 18) {
     console.log(`Comprador maior de idade, pode comprar`);
     listaDeDestinos.splice(1, 1); //utilizamos o metodo splice para remover um valor da lista, no caso o São Paulo
-}else {
-    console.log("Comprador menor de idade, não pode comprar");
-    cosole.log("Teste");
+} else if (estaAcompanhada) {
+    console.log("Comprador é menor de idade mas está acompanhado, pode comprar");
+    listaDeDestinos.splice(1, 1);
+} else {
+    console.log("Não é maior de idade e não pode posso vender");
 }
 
