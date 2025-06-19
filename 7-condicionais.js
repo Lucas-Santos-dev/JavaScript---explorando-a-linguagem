54dd4b6ed2a2d1dba9f9c5a3ec82c7488e1e8f9e
Lucas-Santos-dev <lucassantosrocha889@gmail.com>
2025-06-19 01:05:48 -0300

    Operadores lógicos encadeados

--- 7-condicionais.js
+++ 7-condicionais.js
@@ -8,17 +8,35 @@ const listaDeDestinos = new Array(
 
 const idadeComprador = 18;
 const estaAcompanhada = true;
+const temPassagemComprada = false;
 
 console.log(`Destinos Possíveis`);
 console.log(listaDeDestinos);
 
-if (idadeComprador >= 18) {
+// if (idadeComprador >= 18) {
+//     console.log(`Comprador maior de idade, pode comprar`);
+//     listaDeDestinos.splice(1, 1); //utilizamos o metodo splice para remover um valor da lista, no caso o São Paulo
+// } else if (estaAcompanhada) {
+//     console.log("Comprador é menor de idade mas está acompanhado, pode comprar");
+//     listaDeDestinos.splice(1, 1);
+// } else {
+//     console.log("Não é maior de idade e não pode posso vender");
+// }
+
+// Operador lógico OR (||) é utilizado para verificar se pelo menos uma das condições é verdadeira.
+if (idadeComprador >= 18 || estaAcompanhada == true) {
     console.log(`Comprador maior de idade, pode comprar`);
+    console.log("Boa Viagem!");
     listaDeDestinos.splice(1, 1); //utilizamos o metodo splice para remover um valor da lista, no caso o São Paulo
-} else if (estaAcompanhada) {
-    console.log("Comprador é menor de idade mas está acompanhado, pode comprar");
-    listaDeDestinos.splice(1, 1);
 } else {
     console.log("Não é maior de idade e não pode posso vender");
 }
 
+console.log(`\nEmbarque: \n\n`);
+
+// Operador lógico AND (&&) é utilizado para verificar se todas as condições são verdadeiras.
+if (idadeComprador >= 18 && temPassagemComprada) {
+    console.log(`Boa Viagem!`);
+} else {
+    console.log(`Você não pode embarcar`);
+}
